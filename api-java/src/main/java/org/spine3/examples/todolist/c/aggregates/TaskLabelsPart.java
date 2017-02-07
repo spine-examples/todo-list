ba6496016d22434af41a30772f5b25acb4f74431
illiashepilov <illia.shepilov@teamdev.com>
2017-02-07 18:25:06 +0200

    Remove repeated handler methods.

--- api-java/src/main/java/org/spine3/examples/todolist/c/aggregates/TaskLabelsPart.java
+++ api-java/src/main/java/org/spine3/examples/todolist/c/aggregates/TaskLabelsPart.java
@@ -21,48 +21,34 @@
 package org.spine3.examples.todolist.c.aggregates;
 
 import com.google.protobuf.Message;
-import org.spine3.change.ValueMismatch;
-import org.spine3.examples.todolist.LabelColor;
-import org.spine3.examples.todolist.LabelDetails;
-import org.spine3.examples.todolist.LabelDetailsChange;
+import org.spine3.examples.todolist.LabelIdsList;
 import org.spine3.examples.todolist.TaskDefinition;
 import org.spine3.examples.todolist.TaskId;
-import org.spine3.examples.todolist.TaskLabel;
 import org.spine3.examples.todolist.TaskLabelId;
-import org.spine3.examples.todolist.TaskLabels;
+import org.spine3.examples.todolist.TaskLabelIds;
 import org.spine3.examples.todolist.c.commands.AssignLabelToTask;
-import org.spine3.examples.todolist.c.commands.CreateBasicLabel;
 import org.spine3.examples.todolist.c.commands.RemoveLabelFromTask;
-import org.spine3.examples.todolist.c.commands.UpdateLabelDetails;
 import org.spine3.examples.todolist.c.events.LabelAssignedToTask;
-import org.spine3.examples.todolist.c.events.LabelCreated;
-import org.spine3.examples.todolist.c.events.LabelDetailsUpdated;
 import org.spine3.examples.todolist.c.events.LabelRemovedFromTask;
 import org.spine3.examples.todolist.c.failures.CannotAssignLabelToTask;
 import org.spine3.examples.todolist.c.failures.CannotRemoveLabelFromTask;
-import org.spine3.examples.todolist.c.failures.CannotUpdateLabelDetails;
-import org.spine3.examples.todolist.c.failures.LabelNotFound;
 import org.spine3.server.aggregate.AggregatePart;
 import org.spine3.server.aggregate.Apply;
 import org.spine3.server.command.Assign;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.spine3.examples.todolist.c.aggregates.FailureHelper.throwCannotAssignLabelToTaskFailure;
 import static org.spine3.examples.todolist.c.aggregates.FailureHelper.throwCannotRemoveLabelFromTaskFailure;
-import static org.spine3.examples.todolist.c.aggregates.MismatchHelper.of;
 import static org.spine3.examples.todolist.c.aggregates.TaskFlowValidator.isValidAssignLabelToTaskCommand;
 import static org.spine3.examples.todolist.c.aggregates.TaskFlowValidator.isValidRemoveLabelFromTaskCommand;
 
 /**
  * @author Illia Shepilov
  */
-public class TaskLabelsPart extends AggregatePart<TaskId, TaskLabels, TaskLabels.Builder> {
-
-    private static final int NOT_FOUND = -1;
+public class TaskLabelsPart extends AggregatePart<TaskId, TaskLabelIds, TaskLabelIds.Builder> {
 
     /**
      * {@inheritDoc}
@@ -74,51 +60,11 @@ public class TaskLabelsPart extends AggregatePart<TaskId, TaskLabels, TaskLabels
     }
 
     @Assign
-    List<? extends Message> handle(CreateBasicLabel cmd) {
-        final LabelDetails.Builder labelDetails = LabelDetails.newBuilder()
-                                                              .setTitle(cmd.getLabelTitle());
-        final LabelCreated result = LabelCreated.newBuilder()
-                                                .setId(cmd.getLabelId())
-                                                .setDetails(labelDetails)
-                                                .build();
-        return Collections.singletonList(result);
-    }
-
-    @Assign
-    List<? extends Message> handle(UpdateLabelDetails cmd) throws CannotUpdateLabelDetails, LabelNotFound {
-        final TaskLabels state = getState();
-        final TaskLabelId labelId = cmd.getId();
-        final int index = ensureLabel(labelId);
-        final TaskLabel taskLabel = state.getLabels(index);
-        final LabelDetails actualLabelDetails = LabelDetails.newBuilder()
-                                                            .setColor(taskLabel.getColor())
-                                                            .setTitle(taskLabel.getTitle())
-                                                            .build();
-        final LabelDetailsChange labelDetailsChange = cmd.getLabelDetailsChange();
-        final LabelDetails expectedLabelDetails = labelDetailsChange.getPreviousDetails();
-
-        final boolean isEquals = actualLabelDetails.equals(expectedLabelDetails);
-
-        if (!isEquals) {
-            final LabelDetails newLabelDetails = labelDetailsChange.getNewDetails();
-            final ValueMismatch mismatch = of(expectedLabelDetails, actualLabelDetails, newLabelDetails, getVersion());
-            FailureHelper.throwCannotUpdateLabelDetailsFailure(labelId, mismatch);
-        }
-
-        final LabelDetailsUpdated labelDetailsUpdated = LabelDetailsUpdated.newBuilder()
-                                                                           .setLabelId(labelId)
-                                                                           .setLabelDetailsChange(labelDetailsChange)
-                                                                           .build();
-        final List<? extends Message> result = Collections.singletonList(labelDetailsUpdated);
-        return result;
-    }
-
-    @Assign
     List<? extends Message> handle(RemoveLabelFromTask cmd) throws CannotRemoveLabelFromTask {
         final TaskLabelId labelId = cmd.getLabelId();
         final TaskId taskId = cmd.getId();
 
-        final TaskAggregateRoot root = new TaskAggregateRoot(taskId);
+        final TaskAggregateRoot root = TaskAggregateRoot.get(taskId);
         final TaskDefinition taskDefinitionState = root.getTaskDefinitionState();
         final boolean isValid = isValidRemoveLabelFromTaskCommand(taskDefinitionState.getTaskStatus());
 
@@ -155,82 +101,32 @@ public class TaskLabelsPart extends AggregatePart<TaskId, TaskLabels, TaskLabels
         return result;
     }
 
-    private int getLabelIndex(TaskLabelId labelId) {
-        final List<TaskLabel> commentList = getState().getLabelsList();
-        final Optional<TaskLabel> firstWithId = commentList.stream()
-                                                           .filter(label -> label.getId()
-                                                                                 .equals(labelId))
-                                                           .findFirst();
-        final int result = firstWithId.map(commentList::indexOf)
-                                      .orElse(NOT_FOUND);
-        return result;
-    }
-
-    /**
-     * Makes sure the aggregate has a label with the passed ID.
-     *
-     * @return index of the label
-     * @throws LabelNotFound if the aggregate does not have a comment with the passed ID
-     */
-    private int ensureLabel(TaskLabelId labelId) throws LabelNotFound {
-        final TaskId taskId = getId();
-        final int result = getLabelIndex(labelId);
-        if (result == NOT_FOUND) {
-            throw new LabelNotFound(taskId, labelId);
-        }
-        return result;
-    }
-
-    @Apply
-    private void labelCreated(LabelCreated event) {
-        final TaskLabel taskLabel = TaskLabel.newBuilder()
-                                             .setId(event.getId())
-                                             .setTitle(event.getDetails()
-                                                            .getTitle())
-                                             .setColor(LabelColor.GRAY)
-                                             .build();
-        getBuilder().addLabels(taskLabel);
-    }
-
-    @Apply
-    private void labelDetailsUpdated(LabelDetailsUpdated event) {
-        final LabelDetails newDetails = event.getLabelDetailsChange()
-                                             .getNewDetails();
-        final TaskLabelId labelId = event.getLabelId();
-        final TaskLabel taskLabel = TaskLabel.newBuilder()
-                                             .setId(labelId)
-                                             .setTitle(newDetails.getTitle())
-                                             .setColor(newDetails.getColor())
-                                             .build();
-        final int index = getLabelIndex(labelId);
-        getBuilder().getLabelsBuilder(index)
-                    .mergeFrom(taskLabel);
-    }
-
     @Apply
     private void labelAssignedToTask(LabelAssignedToTask event) {
-        List<TaskLabel> list = getState().getLabelsList()
-                                         .stream()
-                                         .collect(Collectors.toList());
-        final TaskLabel label = TaskLabel.newBuilder()
-                                         .setId(event.getLabelId())
-                                         .build();
-        list.add(label);
-        getBuilder().clearLabels()
-                    .addAllLabels(list);
+        List<TaskLabelId> list = getState().getLabelIdsList()
+                                           .getLabelIdsList()
+                                           .stream()
+                                           .collect(Collectors.toList());
+
+        list.add(event.getLabelId());
+        final LabelIdsList labelIdsList = LabelIdsList.newBuilder()
+                                                      .addAllLabelIds(list)
+                                                      .build();
+        getBuilder().clearLabelIdsList()
+                    .setLabelIdsList(labelIdsList);
     }
 
     @Apply
     private void labelRemovedFromTask(LabelRemovedFromTask event) {
-        List<TaskLabel> list = getState().getLabelsList()
-                                         .stream()
-                                         .collect(Collectors.toList());
-        list.stream()
-            .filter(label -> !label.getId()
-                                   .equals(event.getLabelId()))
-            .collect(Collectors.toList());
-
-        getBuilder().clearLabels()
-                    .addAllLabels(list);
+        List<TaskLabelId> list = getState().getLabelIdsList()
+                                           .getLabelIdsList()
+                                           .stream()
+                                           .collect(Collectors.toList());
+        list.remove(event.getLabelId());
+        final LabelIdsList labelIdsList = LabelIdsList.newBuilder()
+                                                      .addAllLabelIds(list)
+                                                      .build();
+        getBuilder().clearLabelIdsList()
+                    .setLabelIdsList(labelIdsList);
     }
 }
