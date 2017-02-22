29916b2671791ecd0ef0c7af97eb9fb53dc97ce6
illiashepilov <illia.shepilov@teamdev.com>
2017-02-22 18:57:37 +0200

    Create tests for the validator classes.

---
+++ cli/src/test/java/org/spine3/examples/todolist/validator/CommonValidatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.examples.todolist.validator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Illia Shepilov
+ */
+@DisplayName("CommonValidator should")
+class CommonValidatorTest {
+
+    private final Validator validator = new CommonValidator();
+
+    @Test
+    @DisplayName("pass the validation")
+    public void passValidation() {
+        final boolean passed = validator.validate("Some input");
+        assertTrue(passed);
+    }
+
+    @Test
+    @DisplayName("not pass the validation when input is empty")
+    public void notPassValidationWhenInputIsEmpty() {
+        final boolean passed = validator.validate("Some input");
+        assertFalse(passed);
+    }
+
+    @Test
+    @DisplayName("not pass the validation when input is null")
+    public void notPassValidationWhenInputIsNull() {
+        final boolean passed = validator.validate("Some input");
+        assertFalse(passed);
+    }
+}
---
+++ cli/src/test/java/org/spine3/examples/todolist/validator/LabelColorValidatorTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.examples.todolist.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.spine3.examples.todolist.LabelColor;
+
+import java.util.Map;
+
+import static com.google.common.collect.Maps.newHashMap;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Illia Shepilov
+ */
+@DisplayName("LabelColorValidator should")
+class LabelColorValidatorTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        final Map<String, LabelColor> colorMap = newHashMap();
+        colorMap.put("1", LabelColor.RED);
+        validator = new LabelColorValidator(colorMap);
+    }
+
+    @Test
+    @DisplayName("pass the validation")
+    public void passValidation() {
+        final boolean passed = validator.validate("1");
+        assertTrue(passed);
+    }
+
+    @Test
+    @DisplayName("not pass the validation when color map does not contain value by specified key")
+    public void notPassValidation() {
+        final boolean passed = validator.validate("0");
+        assertFalse(passed);
+    }
+}
