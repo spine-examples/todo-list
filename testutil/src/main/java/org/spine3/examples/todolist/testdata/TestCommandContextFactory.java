43091de971b9f58e476ea90b6c388cc20e5b0887
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-04-13 15:18:02 +0300

    Make created `CommandContext` single tenant

--- testutil/src/main/java/org/spine3/examples/todolist/testdata/TestCommandContextFactory.java
+++ testutil/src/main/java/org/spine3/examples/todolist/testdata/TestCommandContextFactory.java
@@ -24,7 +24,6 @@ import com.google.protobuf.Timestamp;
 import org.spine3.base.CommandContext;
 import org.spine3.base.CommandId;
 import org.spine3.base.Commands;
-import org.spine3.users.TenantId;
 import org.spine3.users.UserId;
 
 import static org.spine3.base.Identifiers.newUuid;
@@ -67,9 +66,7 @@ public class TestCommandContextFactory {
                                                              .setCommandId(commandId)
                                                              .setActor(userId)
                                                              .setTimestamp(when)
-                                                             .setZoneOffset(UTC)
-                                                             .setTenantId(TenantId.newBuilder()
-                                                                                  .setValue(newUuid()));
+                                                             .setZoneOffset(UTC);
         return builder.build();
     }
 }
