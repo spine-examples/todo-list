bdd44ea914e7566ab2c3910bb0633d54ccbd45ea
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-07-03 10:47:42 +0300

    Change the `java_package` for the .proto file to the package where it is used

--- cli/src/test/java/io/spine/examples/todolist/view/command/ValidationExceptionFormatterTest.java
+++ cli/src/test/java/io/spine/examples/todolist/view/command/ValidationExceptionFormatterTest.java
@@ -21,8 +21,6 @@
 package io.spine.examples.todolist.view.command;
 
 import com.google.protobuf.Descriptors.FieldDescriptor;
-import io.spine.examples.todolist.test.NaturalNumber;
-import io.spine.examples.todolist.test.NaturalNumberVBuilder;
 import io.spine.validate.ValidationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
