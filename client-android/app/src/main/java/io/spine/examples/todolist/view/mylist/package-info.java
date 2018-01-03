bc00a072656daf77a8a624aabe6760bee2c3fdd9
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2018-01-03 16:41:55 +0200

    Update yet more copyright comments.

--- client-android/app/src/main/java/io/spine/examples/todolist/view/mylist/package-info.java
+++ client-android/app/src/main/java/io/spine/examples/todolist/view/mylist/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, TeamDev Ltd. All rights reserved.
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -24,4 +24,3 @@
 @ParametersAreNonnullByDefault
 package io.spine.examples.todolist.view.mylist;
 
-import javax.annotation.ParametersAreNonnullByDefault;
