ae34cda1025b15352e823caa4f115c54a3a9ba41
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2018-05-14 13:03:28 +0300

    Configure Proto2JS compilation.

---
+++ client-web/webpack.config.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2000-2018 TeamDev Ltd. All rights reserved.
+ * TeamDev PROPRIETARY and CONFIDENTIAL.
+ * Use is subject to license terms.
+ */
+
+let path = require("path");
+
+const config = {
+    target: "web"
+};
+
+module.exports = config;
