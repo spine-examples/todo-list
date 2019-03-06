92e6734366b967519922bd6bddf47ce62d1e0ead
dmitrykuzmin <dmitry.kuzmin@teamdev.com>
2019-03-06 19:20:12 +0200

    Add `FirebaseClient` service

--- client-angular/src/environments/environment.prod.ts
+++ client-angular/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+import {commonEnvironment} from './environment.common';
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: commonEnvironment.firebaseConfig
 };
