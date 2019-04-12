74ef779c21529fe748992ecfe79ee33dae01dfd1
yegor.udovchenko <yegor.udovchenko@teamdev.com>
2019-04-12 10:36:24 +0300

    Add gradle tasks for local Datastore emulator.

---
+++ scripts/start-datastore.bat
@@ -0,0 +1,6 @@
+::Starts the local Google Datastore emulator at the port passed as a parameter.
+::
+::To start the emulator from the Gradle task, do the following:
+::"./scripts/start-datastore.bat 8081".execute()
+::
+gcloud beta emulators datastore start --project=oc-contractors-local --host-port=localhost:%1 --consistency 1.0 --no-store-on-disk
