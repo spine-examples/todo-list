6852ae05d60b0304114c7a8f132195bd7f64c1fb
Serhii Lekariev <serhii.lekariev@teamdev.com>
2019-04-23 20:00:44 +0300

    Fix formatting.

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -41,7 +41,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.layoutService.config$.subscribe(config => {
-      console.log(this.showQuitButton);
       this.showQuitButton = config.showQuitButton;
       this.showNav = config.showNavigation;
       this.toolbarLabel = config.toolbarLabel;
