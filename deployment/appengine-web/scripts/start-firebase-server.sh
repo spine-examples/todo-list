e08d190f866fe2785cd55de14c912e5479c9f7c9
yegor.udovchenko <yegor.udovchenko@teamdev.com>
2019-05-03 14:17:12 +0300

    Add scripts for emulating Firebase server locally.

--- deployment/appengine-web/scripts/start-firebase-server.sh
+++ deployment/appengine-web/scripts/start-firebase-server.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 #
-# Starts the local Firebase server emulator at the local port passed as a parameter.
+# Starts the local Firebase server emulator with enabled REST API. Starts at the given local port
+# from the first parameter, applying Firebase Realtime Database rules taken from the path, passed
+# as a second parameter.
 #
-# To start the emulator from the Gradle task, do the following:
-# "./scripts/start-firebase-server.sh 8082".execute()
+# To start the emulator from the Gradle task at port 9999 with Firebase rules form
+# `todo-list/example-firebase-rules.json`, do the following:
 #
-firebase-server -p $1 -e -b
+# "./scripts/start-firebase-server.sh 9999 ./example-firebase-rules.json".execute()
+#
+firebase-server --port $1 --rules $2 --rest
