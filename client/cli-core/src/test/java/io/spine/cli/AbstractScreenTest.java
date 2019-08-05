db804049c74dc5c1b025a763aadc119598536dcc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-08-05 16:53:53 +0300

    Restore code removed during module move

---
+++ client/cli-core/src/test/java/io/spine/cli/AbstractScreenTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.cli;
+
+import io.spine.cli.action.Shortcut;
+import io.spine.cli.action.TransitionAction;
+import io.spine.cli.view.View;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("AbstractScreen should")
+class AbstractScreenTest {
+
+    private static final String BACK_NAME = "back";
+    private static final Shortcut BACK_SHORTCUT = new Shortcut("b");
+
+    private final AbstractScreen screen = new AnAbstractScreen();
+
+    @Test
+    @DisplayName("render a view")
+    void renderView() {
+        NoOpView view = new NoOpView();
+        screen.renderView(view);
+        assertTrue(view.wasRendered());
+    }
+
+    @Test
+    @DisplayName("not modify history on a current view rendering")
+    void notModifyHistoryOnCurrentViewRendering() {
+        View view = new NoOpView();
+
+        screen.renderView(view);
+        List<View> expectedHistory = screen.getHistory();
+
+        screen.renderView(view);
+        assertEquals(expectedHistory, screen.getHistory());
+    }
+
+    @Test
+    @DisplayName("make previous view current on its rendering")
+    void makePreviousViewCurrent() {
+        View first = new NoOpView();
+        View second = new NoOpView();
+
+        screen.renderView(first);
+        screen.renderView(second);
+
+        List<View> history = screen.getHistory();
+        List<View> historyWithoutCurrentView =
+                newArrayList(history.subList(0, history.size() - 1));
+
+        screen.renderView(first);
+
+        assertEquals(historyWithoutCurrentView, screen.getHistory());
+    }
+
+    @Test
+    @DisplayName("not create back action if there is no back destination")
+    void notCreateBackAction() {
+        Optional<TransitionAction<View, View>> back = screen.createBackAction(BACK_NAME,
+                                                                              BACK_SHORTCUT);
+        assertFalse(back.isPresent());
+
+        View view = new NoOpView();
+        screen.renderView(view);
+        back = screen.createBackAction(BACK_NAME, BACK_SHORTCUT);
+        assertFalse(back.isPresent());
+    }
+
+    @SuppressWarnings("OptionalGetWithoutIsPresent") // In test we know for sure it's there.
+    @Test
+    @DisplayName("create back action if there is back destination")
+    void createBackAction() {
+        View first = new NoOpView();
+        View second = new NoOpView();
+        View third = new NoOpView();
+
+        screen.renderView(first);
+        screen.renderView(second);
+        screen.renderView(third);
+
+        TransitionAction<View, View> back = screen.createBackAction(BACK_NAME, BACK_SHORTCUT)
+                                                  .get();
+        assertEquals(BACK_NAME, back.getName());
+        assertEquals(BACK_SHORTCUT, back.getShortcut());
+        assertEquals(third, back.getSource());
+        assertEquals(second, back.getDestination());
+    }
+
+    @Test
+    @DisplayName("create back action and does not modify view history")
+    void createBackAndNotModifyHistory() {
+        View first = new NoOpView();
+        View second = new NoOpView();
+        screen.renderView(first);
+        screen.renderView(second);
+
+        List<View> expectedHistory = screen.getHistory();
+        screen.createBackAction(BACK_NAME, BACK_SHORTCUT);
+        assertEquals(expectedHistory, screen.getHistory());
+    }
+
+    private static class AnAbstractScreen extends AbstractScreen {
+
+        private static final String UNSUPPORTED_MSG = "Not implemented in AbstractScreen.";
+
+        @Override
+        public String promptUser(String prompt) {
+            throw new UnsupportedOperationException(UNSUPPORTED_MSG);
+        }
+
+        @Override
+        public void println(String message) {
+            throw new UnsupportedOperationException(UNSUPPORTED_MSG);
+        }
+    }
+}
---
+++ client/cli-core/src/test/java/io/spine/cli/action/CommandActionTest.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2019, TeamDev. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.cli.action;
+
+import io.spine.cli.Bot;
+import io.spine.cli.CreateProject;
+import io.spine.cli.ProjectId;
+import io.spine.cli.Screen;
+import io.spine.cli.view.CommandView;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.spine.cli.NoOpAction.noOpActionProducer;
+import static io.spine.validate.Validate.isDefault;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("CommandAction should")
+class CommandActionTest {
+
+    private static final String ACTION_NAME = "quit";
+    private static final Shortcut QUIT = new Shortcut("q");
+
+    private final CreateProjectView view = new CreateProjectView();
+    private final CreateProjectAction action = new CreateProjectAction(view);
+
+    @BeforeEach
+    void setUp() {
+        view.addAction(noOpActionProducer(ACTION_NAME, QUIT));
+        new Bot().addAnswer(QUIT.getValue());
+    }
+
+    @Test
+    @DisplayName("have same source and destination view")
+    void haveSameSourceAndDestination() {
+        assertSame(action.getSource(), action.getDestination());
+    }
+
+    @Test
+    @DisplayName("execute command and render a source view")
+    void executeCommandAndRenderSource() {
+        ProjectId expectedId = ProjectId
+                .newBuilder()
+                .setValue("Some ID")
+                .build();
+        CreateProject.Builder viewState = view.getState();
+        viewState.setProjectId(expectedId);
+        action.execute();
+
+        assertEquals(expectedId, action.getCommandMessageBeforeExecution()
+                                       .getProjectId());
+        assertTrue(view.wasRendered());
+    }
+
+    @Test
+    @DisplayName("clear state of source view after successful execution")
+    void clearSourceState() {
+        ProjectId nonDefaultId = ProjectId
+                .newBuilder()
+                .setValue("Non-default ID")
+                .build();
+        CreateProject.Builder viewState = view.getState();
+        viewState.setProjectId(nonDefaultId);
+        action.execute();
+        assertTrue(isDefault(viewState.buildPartial()));
+    }
+
+    private static class CreateProjectAction
+            extends CommandAction<CreateProject, CreateProject.Builder> {
+
+        private CreateProject commandMessageBeforeExecution;
+
+        private CreateProjectAction(CommandView<CreateProject, CreateProject.Builder> source) {
+            super(source);
+        }
+
+        @Override
+        protected void post(CreateProject commandMessage) {
+            commandMessageBeforeExecution = commandMessage;
+        }
+
+        private CreateProject getCommandMessageBeforeExecution() {
+            return commandMessageBeforeExecution;
+        }
+    }
+
+    private static class CreateProjectView
+            extends CommandView<CreateProject, CreateProject.Builder> {
+
+        private boolean rendered;
+
+        private CreateProjectView() {
+            super("View title");
+        }
+
+        @Override
+        protected void renderBody(Screen screen) {
+            rendered = true;
+        }
+
+        @Override
+        protected String renderState(CreateProject.Builder state) {
+            return String.valueOf(rendered);
+        }
+
+        private boolean wasRendered() {
+            return rendered;
+        }
+    }
+}
